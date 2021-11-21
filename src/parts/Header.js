24dcfa841249bdb10616c260201a23aec28f87c0
sinatriiobimo <sinatriobimbim@gmail.com>
2021-11-21 07:09:53 -0800

    feat(master): create header and icontext

---
+++ src/parts/Header.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Button from 'elements/Button';
+import BrandIcon from 'parts/IconText';
+
+export default function Header(props) {
+    const getNavLinkClass = path => {
+        return props.location.pathname === path ? " active" : "";
+    }
+
+    return (
+        <header className="spacing-sm">
+            <div className="container">
+                <nav className="navbar navbar-expand-lg navbar-light">
+                    <BrandIcon/>
+                    <div className="collapse navbar-collapse">
+                        <ul className="navbar-nav ml-auto">
+                            <li className={`nav-item${getNavLinkClass('/')}`}>
+                                <Button className="nav-link" type="link" href="/">Beranda</Button>
+                            </li>
+                            <li className={`nav-item${getNavLinkClass('/berita')}`}>
+                                <Button className="nav-link" type="link" href="/berita">Berita</Button>
+                            </li>
+                            <li className={`nav-item${getNavLinkClass('/fakultas')}`}>
+                                <Button className="nav-link" type="link" href="/fakultas">Fakultas</Button>
+                            </li>
+                            <li className={`nav-item${getNavLinkClass('/kontak')}`}>
+                                <Button className="nav-link" type="link" href="/kontak">Kontak</Button>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        </header>
+    )
+}
